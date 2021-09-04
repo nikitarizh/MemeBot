323743775a79fd9ac1106fba4845f2b72673d413
nikitarizh <nrigworks@gmail.com>
2021-09-05 02:42:17 +0300

    feat: add exception handling

--- src/main/java/com/nikitarizh/memebot/service/ExecutionService.java
+++ src/main/java/com/nikitarizh/memebot/service/ExecutionService.java
@@ -1,29 +1,29 @@
 package com.nikitarizh.memebot.service;
 
-import com.pengrad.telegrambot.TelegramBot;
+import com.nikitarizh.memebot.entity.BotRequestEntity;
+import com.nikitarizh.memebot.exception.NoActionException;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendPhoto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ExecutionService {
 
-    private final TelegramBot bot;
-    private final MemeService memeService;
+    private final ActionDispatcherService actionDispatcherService;
 
-    public void executeAll(List<Update> updates) {
-        updates.forEach(this::execute);
-    }
+    public void execute(Update update, BotRequestEntity botRequest) {
+        var messageText = update.message().text();
 
-    public void execute(Update update) {
-        var messageRequest = new SendPhoto(update.message().chat().id(), memeService.getMemeUrl());
-        var response = bot.execute(messageRequest);
-        log.info(response.toString());
+        try {
+            actionDispatcherService.getActionForCommand(messageText).act(update, botRequest);
+        } catch (NoActionException e) {
+            actionDispatcherService.getActionForCommand("help").act(update, botRequest);
+        } catch (Exception e) {
+            log.error("Error occurred while execution", e);
+            actionDispatcherService.getActionForCommand("error").act(update, botRequest);
+        }
     }
 }
